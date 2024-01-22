ac56fe231ba585739b40a6ed3af84838f9ea2075
ratul544388 <ratulhossainbnc@gmail.com>
2024-01-22 19:09:09 +0600

    fix

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -3,7 +3,11 @@
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { Button } from "./ui/button";
 import { ChevronsLeft, ChevronsRight } from "lucide-react";
+import ReactPaginate from "react-paginate";
 import qs from "query-string";
+import { useEffect, useState } from "react";
+import { cn } from "@/lib/utils";
+import { Loader } from "./loader";
 
 interface PaginationProps {
   currentPage: number;
@@ -14,6 +18,7 @@ export const Pagination = ({ currentPage, totalPages }: PaginationProps) => {
   const pathname = usePathname();
   const router = useRouter();
   const params = useSearchParams();
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleClick = (page: number) => {
     const currentQuery = qs.parse(params.toString());
@@ -25,30 +30,47 @@ export const Pagination = ({ currentPage, totalPages }: PaginationProps) => {
       },
     });
 
-    router.push(url);
+    router.push(url, { scroll: false });
   };
 
+  const handlePageClick = (event: { selected: number }) => {
+    setIsLoading(true);
+    const newPageNumber = event.selected + 1;
+    handleClick(newPageNumber);
+  };
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, [params]);
+
   return (
-    <div className="flex items-center gap-3 w-fit ml-auto">
-      <Button
-        disabled={currentPage === 1}
-        onClick={() => handleClick(currentPage - 1)}
-        className="bg-neutral-900 hover:bg-neutral-900/90 dark:bg-slate-100 dark:hover:bg-slate-100/90 dark:text-neutral-900"
-      >
-        <ChevronsLeft className="h-4 w-4 mr-2" />
-        Prev
-      </Button>
-      <div className="min-w-[110px] flex justify-center text-sm text-muted-foreground">
-        {currentPage} of {totalPages} pages
-      </div>
-      <Button
-        disabled={currentPage === totalPages}
-        onClick={() => handleClick(currentPage + 1)}
-        className="bg-neutral-900 hover:bg-neutral-900/90 dark:bg-slate-100 dark:hover:bg-slate-100/90 dark:text-neutral-900"
-      >
-        Next
-        <ChevronsRight className="h-4 w-4 ml-2" />
-      </Button>
-    </div>
+    <>
+      <ReactPaginate
+        className="flex gap-5 font-semibold text-foreground/60 items-center flex-wrap justify-center bg-accent w-fit mx-auto rounded-lg py-2 px-3 transition-all"
+        breakLabel="..."
+        breakClassName=""
+        nextLabel=">"
+        nextClassName={cn(
+          "font-extrabold",
+          currentPage === totalPages && "pointer-events-none opacity-50"
+        )}
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={3}
+        pageCount={totalPages}
+        activeClassName="bg-primary px-3 py-1 text-white rounded-md"
+        pageClassName=""
+        previousLabel="<"
+        previousClassName={cn(
+          "font-extrabold",
+          currentPage === 1 && "pointer-events-none opacity-50"
+        )}
+        renderOnZeroPageCount={null}
+      />
+      {isLoading && (
+        <div className="fixed inset-0">
+          <Loader />
+        </div>
+      )}
+    </>
   );
 };
